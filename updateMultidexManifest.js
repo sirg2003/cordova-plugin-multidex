2a5ce33b39c94c85d9e344734e79ae8b527ff588
Sir_G <sirg2003@gmail.com>
2017-11-06 20:31:59 +1100

    Update updateMultidexManifest.js
    add comments

--- updateMultidexManifest.js
+++ updateMultidexManifest.js
@@ -8,8 +8,12 @@ module.exports = function(ctx) {
     if (doc.getroot().tag !== 'manifest') {
         throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
     }
-
+    
+    //adds the tools namespace to the root node
+    // doc.getroot().attrib['xmlns:tools'] = 'http://schemas.android.com/tools';
+    //add tools:replace in the application node
     doc.getroot().find('./application').attrib['android:name'] = 'android.support.multidex.MultiDexApplication';
-
+    
+    //write the manifest file
     fs.writeFileSync(manifestPath, doc.write({indent: 4}), 'utf-8');
 };
